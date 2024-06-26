2eb5453e6e7f53f849c2cc3462d7ae05c69db3e5
YellowAfterlife <yellowafterlife@hotmail.com>
2024-06-26 03:11:51 +0300

    Hello!

---
+++ README.md
@@ -0,0 +1,8 @@
+# gmk-snips-run
+
+This little plugin enables running [gmk-snippets](https://github.com/YAL-GameMaker/gmk-snippets)
+projects (open a `.gmk-snips` in GMEdit) by pressing F5 in the code editor.
+
+stdout/stderr are logged into the JS console, for better or worse.
+
+A single preference sets up the snippet-tester.exe path.
---
+++ gmk-snips-run.js
@@ -0,0 +1,78 @@
+(function() {
+	const child_process = require("child_process");
+	const Preferences = $gmedit["ui.Preferences"];
+	const Dialog = $gmedit['electron.Dialog'];
+	function run() {
+		if ($gmedit['gml.GmlAPI'].version.name != "gmk-snip") return;
+		
+		console.log("Starting the runner...");
+		let runnerPath = Preferences.current.gmkSnipTesterPath;
+		if (runnerPath == null) {
+			Dialog.showError("Please set up snippet-tester path in Preferences!");
+			return;
+		}
+		if (!Electron_FS.existsSync(runnerPath)) {
+			Dialog.showError(`snippet-tester path "${runnerPath}" doesn't exist!`);
+			return;
+		}
+		let listFilePath = $gmedit['gml.Project'].current.path;
+		listFilePath = listFilePath.split("/").join("\\");
+		let runnerArgs = [
+			listFilePath,
+		];
+		let runner = child_process.spawn(runnerPath, runnerArgs);
+		runner.on("spawn", () => {
+			console.log("Started up the runner!");
+		});
+		runner.stdout.on("data", (e) => {
+            let text = e.toString();
+            console.log(text);
+        });
+        runner.stderr.on("data", (e) => {
+            let text = e.toString();
+			console.error(text);
+        });
+		runner.on("close", (exitCode) => {
+			exitCode ??= 0;
+			runner = null;
+			console.log(`Runner closed, exitCode=${exitCode} (0x${exitCode.toString(16)})`);
+		})
+	}
+	function initCommands() {
+		const commands = [{
+            name: "gmk-snips-run",
+            title: "gmk-snips: Run",
+            bindKey: "F5",
+            exec: run,
+        }];
+        
+        let hashHandler = $gmedit["ui.KeyboardShortcuts"].hashHandler;
+        let AceCommands = $gmedit["ace.AceCommands"];
+        for (let cmd of commands) {
+            hashHandler.addCommand(cmd);
+            AceCommands.add(cmd);
+            AceCommands.addToPalette({
+                name: cmd.title,
+                exec: cmd.name,
+            })
+        }
+	}
+	function initPreferences(e) {
+		let out = e.target.querySelector('.plugin-settings[for="gmk-snips-run"]');
+		let pathEl = Preferences.addInput(out,
+			"Path to snippet-tester.exe",
+			Preferences.current.gmkSnipTesterPath || "",
+		function(str) {
+			Preferences.current.gmkSnipTesterPath = str;
+			Preferences.save();
+		});
+		pathEl.querySelector("input").placeholder = "C:\\snippet_tester.exe";
+	}
+	//
+	GMEdit.register("gmk-snips-run", {
+		init: function(config) {
+			initCommands();
+			GMEdit.on("preferencesBuilt", initPreferences);
+		}
+	});
+})();
